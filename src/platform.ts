84cab42698a7ca26b8abd88612b11ede0a0083fc
bbernstein <bernard@thebernsteins.com>
2022-12-20 17:20:24 -0500

    issue-14 added lint ignore

--- src/platform.ts
+++ src/platform.ts
@@ -69,6 +69,8 @@ export class SnowSensePlatform implements DynamicPlatformPlugin {
     const watcher = SnowWatch.getInstance();
     try {
       await watcher.updatePredictionStatus();
+
+      // eslint-disable-next-line @typescript-eslint/no-explicit-any
     } catch (e: any) {
       that.log.error(`Error getting updated weather: ${e.message}`);
       return;
