6b8abb74348c16081c4feb5096576f73ad54c9e6
Bernie Bernstein <2228270+bbernstein@users.noreply.github.com>
2023-03-14 14:52:23 -0400

    Issue 23 - Enable multiple sensors with varied settings (#24)
    * cleanup-2 cleanup config files
    
    * 2.0.5-beta.0
    
    * cleanup-2 cleanup config and README updates
    
    * 2.0.6-beta.0
    
    * cleanup-2 cleanup config and README updates
    
    * 2.0.5-beta.1
    
    * cleanup-2 add notes to self in README
    
    * 2.0.5-beta.2
    
    * issue-20 Implemented onlyWhenCold and coldTempThreshold
    
    * 2.0.5-beta.3
    
    * issue-20 Implemented onlyWhenCold and coldTempThreshold
    
    * issue-20 Updated config settings
    
    * issue-20 added prepublishOnly to readme
    
    * 2.0.5-beta.4
    
    * issue-20 updated .npmignnore
    
    * 2.0.5-beta.5
    
    * issue-20 Added config and logic for consecutiveHoursOfSnowIsSnowy
    
    * 2.0.5-beta.6
    
    * issue-20 cleaned and simplified tests with helper funcs
    
    * issue-20 100% test coverage for SnowForecast and SnowWatch
    
    * issue-20 Fixed broken test (typo)
    
    * issue-20 added tests for 0-before 0-after
    
    * 2.0.5-beta.7
    
    * issue-23 Allow crating multiple sensors each with different settings
    
    * issue-23 removed commented-out changes
    
    * 2.0.5-beta.8
    
    * issue-23 fixed displayName config
    
    * 2.0.5-beta.9
    
    * issue-23 save history to file to recover after restarts
    
    * 2.0.5-beta.10
    
    * issue-23 updated README and config cleanup
    
    * 2.0.5-beta.11
    
    * issue-23 cleaned up debug messages showing snow status
    
    * 2.0.5-beta.12
    
    * issue-23 minor debug changes
    
    * 2.0.5-beta.13
    
    * 2.0.5
    
    * issue-23 cleaned merge

--- src/SnowSenseConfig.ts
+++ src/SnowSenseConfig.ts
@@ -1,4 +1,13 @@
 import {PlatformConfig} from 'homebridge/lib/bridgeService';
+import {readFileSync, writeFileSync} from 'fs';
+import {Logger} from 'homebridge';
+
+export interface DeviceConfig {
+  displayName: string;
+  hoursBeforeSnowIsSnowy: number;
+  hoursAfterSnowIsSnowy: number;
+  consecutiveHoursFutureIsSnowy: number;
+}
 
 export interface SnowSenseConfig extends PlatformConfig {
   apiKey: string;
@@ -7,18 +16,146 @@ export interface SnowSenseConfig extends PlatformConfig {
   debugOn: boolean;
   units?: 'imperial' | 'metric' | 'standard';
   location: string;
-  hoursBeforeSnowIsSnowy: number;
-  hoursAfterSnowIsSnowy: number;
   coldPrecipitationThreshold?: number;
   onlyWhenCold: boolean;
   coldTemperatureThreshold?: number;
-  consecutiveHoursOfSnowIsSnowy: number;
+  sensors?: DeviceConfig[];
 
   // old configs
+  hoursBeforeSnowIsSnowy: number;
+  hoursAfterSnowIsSnowy: number;
+  consecutiveHoursOfSnowIsSnowy: number;
   latitude?: number;
   longitude?: number;
   afterSnowStops?: number;
   beforeSnowStarts?: number;
   forecastFrequency?: number;
   key?: string;
+}
+
+
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
+function deepEqual(obj1: any, obj2: any): boolean {
+  // Check if the objects are arrays
+  const isArr1 = Array.isArray(obj1);
+  const isArr2 = Array.isArray(obj2);
+  if (isArr1 && isArr2) {
+    // If both objects are arrays, compare each element
+    if (obj1.length !== obj2.length) {
+      return false;
+    }
+    for (let i = 0; i < obj1.length; i++) {
+      if (!deepEqual(obj1[i], obj2[i])) {
+        return false;
+      }
+    }
+    return true;
+  }
+  if (isArr1 !== isArr2) {
+    // If one object is an array and the other is not, they are not equal
+    return false;
+  }
+  // If the objects are not arrays, continue with the original comparison
+  const keys1 = Object.keys(obj1);
+  const keys2 = Object.keys(obj2);
+  if (keys1.length !== keys2.length) {
+    return false;
+  }
+  for (const key of keys1) {
+    const val1 = obj1[key];
+    const val2 = obj2[key];
+    if (typeof val1 === 'object' && typeof val2 === 'object') {
+      if (!deepEqual(val1, val2)) {
+        return false;
+      }
+    } else if (val1 !== val2) {
+      return false;
+    }
+  }
+  return true;
+}
+
+export function upgradeConfigs(config: SnowSenseConfig, configPath: string, logger: Logger) {
+  let configChanged = false;
+  // read legacy configs, and see if anything changed
+  if (!config.sensors
+    && config.name
+    && config.hoursAfterSnowIsSnowy
+    && config.hoursBeforeSnowIsSnowy
+    && config.consecutiveHoursOfSnowIsSnowy) {
+    config.sensors = [{
+      displayName: config.name,
+      hoursBeforeSnowIsSnowy: config.hoursBeforeSnowIsSnowy,
+      hoursAfterSnowIsSnowy: config.hoursAfterSnowIsSnowy,
+      consecutiveHoursFutureIsSnowy: config.consecutiveHoursOfSnowIsSnowy,
+    }];
+    configChanged = true;
+  }
+  if (config.sensors && config.sensors.length > 0) {
+    const newSensors = config.sensors
+      .map((sensor: DeviceConfig): DeviceConfig => ({
+        displayName: sensor.displayName || 'Snowy-' + Math.random().toString(36).substring(7),
+        hoursBeforeSnowIsSnowy: sensor.hoursBeforeSnowIsSnowy,
+        hoursAfterSnowIsSnowy: sensor.hoursAfterSnowIsSnowy,
+        consecutiveHoursFutureIsSnowy: sensor.consecutiveHoursFutureIsSnowy,
+      }));
+    const sensorChanged = !deepEqual(config.sensors, newSensors);
+    if (sensorChanged) {
+      config.sensors = newSensors;
+      configChanged = true;
+    }
+  }
+  if (!config.apiKey && config.key) {
+    config.apiKey = config.key;
+    config.key = undefined;
+    // if we had an old key, it was version v2.5, so let that keep working
+    config.apiVersion = '2.5';
+    configChanged = true;
+  }
+  if (!config.apiVersion) {
+    config.apiVersion = '3.0';
+    configChanged = true;
+  }
+  if (!config.location && config.latitude && config.longitude) {
+    config.location = `${config.latitude},${config.longitude}`;
+    config.latitude = undefined;
+    config.longitude = undefined;
+    configChanged = true;
+  }
+  if (config.units && !config.units.match(/^(imperial|metric)$/)) {
+    config.units = 'imperial';
+    configChanged = true;
+  }
+  if (!config.hoursBeforeSnowIsSnowy && config.beforeSnowStarts) {
+    config.hoursBeforeSnowIsSnowy = config.beforeSnowStarts;
+    config.beforeSnowStarts = undefined;
+    configChanged = true;
+  }
+  if (!config.hoursAfterSnowIsSnowy && config.afterSnowStops) {
+    config.hoursAfterSnowIsSnowy = config.afterSnowStops;
+    config.afterSnowStops = undefined;
+    configChanged = true;
+  }
+  if (!config.apiThrottleMinutes && config.forecastFrequency) {
+    config.apiThrottleMinutes = config.forecastFrequency;
+    config.forecastFrequency = undefined;
+    configChanged = true;
+  }
+  if (configChanged) {
+    try {
+      logger.info('Updating config to new format. ', config);
+      const allConfigs = JSON.parse(readFileSync(configPath, 'utf8'));
+      // find the platform entry matching this platform
+      // eslint-disable-next-line @typescript-eslint/no-explicit-any
+      const platformIndex = allConfigs.platforms.findIndex((c: any) => c.platform === config.platform);
+      if (platformIndex >= 0) {
+        // if it was found, replace that entry with the updated config and write it
+        allConfigs.platforms[platformIndex] = config;
+        writeFileSync(configPath, JSON.stringify(allConfigs, null, 4), 'utf8');
+      }
+      // eslint-disable-next-line @typescript-eslint/no-explicit-any
+    } catch (e: any) {
+      logger.error(`Error updating config file: ${e}`);
+    }
+  }
 }
